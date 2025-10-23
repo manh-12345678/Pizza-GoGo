a9ebd2e10ab8ff04a55bc85ae10f5aafa402ccb2
PhamHai <phamthaihai2272004@gmail.com>
2025-10-23 23:59:27 +0700

    Add login and register

--- src/main/java/Group5_pizza/Pizza_GoGo/repository/RestaurantTableRepository.java
+++
@@ -1,7 +0,0 @@
-package Group5_pizza.Pizza_GoGo.repository;
-
-import Group5_pizza.Pizza_GoGo.model.RestaurantTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Integer> {
-}
