9f382ef023ca4d86b773912dab7036b41daa14b5
manh-12345678 <220846999+manh-12345678@users.noreply.github.com>
2025-10-28 00:26:36 +0700

    update Homepage and profile

--- src/main/java/Group5_pizza/Pizza_GoGo/model/Account.java
+++ src/main/java/Group5_pizza/Pizza_GoGo/model/Account.java
@@ -1,8 +1,22 @@
 package Group5_pizza.Pizza_GoGo.model;
 
-import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "Account")
@@ -10,6 +24,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "UserId")
@@ -24,16 +39,42 @@ public class Account {
     @Column(name = "FullName", length = 100)
     private String fullName;
 
+    @Column(name = "Email", nullable = false, unique = true, length = 100)  // Thêm trường email
+    private String email;
+
     @ManyToOne
     @JoinColumn(name = "RoleId", nullable = false)
+    @ToString.Exclude
+    @JsonBackReference
     private Role role;
 
-    @Column(name = "CreatedAt", columnDefinition = "DATETIME DEFAULT GETDATE()")
+    @Column(name = "CreatedAt", updatable = false, columnDefinition = "DATETIME DEFAULT GETDATE()")
     private LocalDateTime createdAt;
 
     @Column(name = "UpdatedAt")
     private LocalDateTime updatedAt;
 
+    @OneToOne
+    @JoinColumn(name = "CustomerId", unique = true)
+    @ToString.Exclude
+    @JsonBackReference
+    private Customer customer;
+
     @Column(name = "IsDeleted", columnDefinition = "BIT DEFAULT 0")
-    private Boolean isDeleted;
+    private Boolean isDeleted = false;
+
+    @Column(name = "IsConfirmed", columnDefinition = "BIT DEFAULT 0")  // Thêm flag xác nhận email
+    private Boolean isConfirmed = false;
+
+    public Account(LocalDateTime createdAt, Customer customer, String email, String fullName, String passwordHash, Role role, LocalDateTime updatedAt, Integer userId, String username) {
+        this.createdAt = createdAt;
+        this.customer = customer;
+        this.email = email;
+        this.fullName = fullName;
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.updatedAt = updatedAt;
+        this.userId = userId;
+        this.username = username;
+    }
 }
